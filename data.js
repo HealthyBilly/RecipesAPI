f42e7692414664d27b3744c8115d2b83bf26d4e2
ChhuongLe <chhuongcle@gmail.com>
2021-07-12 11:11:50 -0700

    add comments to clarify

--- data.js
+++ data.js
@@ -9,11 +9,16 @@ const info = [];
 // const req2 = axios.get('https://api.spoonacular.com/recipes/findByNutrients')
 
 axios.all(diets.map(name => {
+  // get recipes for each of the diets listed in the diets array
   axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&diet=${name}`)
     .then(res => {
+      // in recipes object, store the name of the recipe and the recipe itself
       recipes[name] = res.results;
+      // store all the recipe names into an array
       const idArr = Object.keys(recipes);
+      // join them all with a comma in between
       const strId = idArr.join(",");
+      // Perform a mass get of each of the recipes for their ingredients
       // TODO: ADD method to store data
       axios.get(`https://api.spoonacular.com/recipes/informationBulk?apiKey=${apiKey}&ids=${strId}`)
         .then(res => {
